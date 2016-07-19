4a78dcccc7a7936d9a837463adc4f6d07dad8405
Cesare Vinciguerra <cesare.vinciguerra@stud.unifi.it>
2016-07-19 21:58:54 +0200

    add name as constructor parameter

--- RPINode/src/MotorDevice.cpp
+++ RPINode/src/MotorDevice.cpp
@@ -11,12 +11,13 @@
 
 BaseCommand* motorOn;
 
-MotorDevice::MotorDevice():IDevice() {
+MotorDevice::MotorDevice(string name):IDevice() {
 	// TODO Auto-generated constructor stub
-	this->m_name = "Motor";
+	this->m_name = name;
 	TCommandSchema schema;// = {{"command1","int"},{"command2","string"}};
 	schema.insert(TCommandSchemaPair("direction","string"));
 	motorOn = new BaseCommand("motorOn",this->m_name,schema);
+	this->addCommand(motorOn);
 }
 
 MotorDevice::~MotorDevice() {
