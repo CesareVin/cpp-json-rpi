d0d8413c70526e8fe1f7f92658b46dceccf45a35
Cesare Vinciguerra <cesare.vinciguerra@stud.unifi.it>
2016-07-20 21:22:27 +0200

    merge icommand with baseCommand

--- CPLCore/src/BaseCommand.cpp
+++ CPLCore/src/BaseCommand.cpp
@@ -7,19 +7,58 @@
 
 #include "BaseCommand.h"
 
-BaseCommand::BaseCommand(string name,string device,map<string,string> schema){
-	m_name = name;
-	m_device = device;
-	m_requestSchema = schema;
-
-}
-BaseCommand::BaseCommand()
+BaseCommand::BaseCommand(string name,string device,map<string,string> schema):m_name(name),
+																			  m_device(device),
+																			  m_requestSchema(schema)
 {
-	m_name = "";
-	m_device = "";
+
 }
 
 BaseCommand::~BaseCommand() {
-	// TODO Auto-generated destructor stub
+}
+
+string BaseCommand::getName()
+{
+	return m_name;
+}
+void BaseCommand::setName(string name)
+{
+	m_name = name;
+}
+
+string BaseCommand::getDevice()
+{
+	return m_device;
+}
+void BaseCommand::setDevice(string device)
+{
+	m_device = device;
+}
+
+void BaseCommand::setRequestSchema(TCommandSchema schema)
+{
+	m_requestSchema = schema;
+}
+map<string,string> BaseCommand::getRequestSchema()
+{
+	return m_requestSchema;
+}
+
+web::json::value BaseCommand::AsJSON()
+{
+	web::json::value res = web::json::value::object();
+	res[U("name")] = web::json::value::string(m_name);
+
+	web::json::value jSchema = web::json::value::array(m_requestSchema.size());
+
+	int idx = 0;
+	for (auto iter = m_requestSchema.begin(); iter != m_requestSchema.end(); ++iter)
+	{
+		web::json::value jSchemaEntity = web::json::value::object();
+		jSchemaEntity[U(iter->first)] = web::json::value::string(iter->second);
+		jSchema[idx++] = jSchemaEntity;
+	}
+	res["RequestSchema"] = jSchema;
+	return res;
 }
 
--- CPLCore/src/BaseCommand.h
+++ CPLCore/src/BaseCommand.h
@@ -8,17 +8,59 @@
 #ifndef BASECOMMAND_H_
 #define BASECOMMAND_H_
 
-#include "ICommand.h"
-
 #include<string>
 #include<vector>
 #include<map>
 
-class BaseCommand: public ICommand {
+
+#ifdef _WIN32
+#define NOMINMAX
+#include <Windows.h>
+#else
+# include <sys/time.h>
+#endif
+#include "cpprest/json.h"
+#include "cpprest/http_listener.h"
+#include "cpprest/uri.h"
+#include "cpprest/asyncrt_utils.h"
+
+using namespace std;
+using namespace web;
+using namespace utility;
+using namespace http;
+using namespace web::http::experimental::listener;
+
+using namespace std;
+
+typedef std::map<std::string, std::string> TCommandSchema;
+typedef std::pair<std::string, std::string> TCommandSchemaPair;
+typedef std::pair<string,string> TCommandParameter;
+
+
+class BaseCommand
+{
 public:
-	BaseCommand(string name,string device,map<string,string> schema);
-	BaseCommand();
+	explicit BaseCommand(string name,string device,map<string,string> schema);
 	virtual ~BaseCommand();
+
+	virtual string getName();
+	virtual void setName(string name);
+
+	virtual string getDevice();
+	virtual void setDevice(string device);
+
+	virtual void setRequestSchema(TCommandSchema schema);
+	virtual map<string,string> getRequestSchema();
+
+	virtual web::json::value AsJSON();
+
+protected:
+
+	string m_name;
+	string m_device;
+	TCommandSchema m_requestSchema;
+	TCommandSchema m_responseSchema;
+	TCommandParameter m_parameters;
 };
 
 #endif /* BASECOMMAND_H_ */
