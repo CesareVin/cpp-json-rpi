cfcb73b4ce488ec5b5e17f064c910fe79e844439
Cesare Vinciguerra <cesare.vinciguerra@stud.unifi.it>
2016-07-21 17:43:44 +0200

    Motor can now manage pins

--- RPINode/src/MotorDevice.cpp
+++ RPINode/src/MotorDevice.cpp
@@ -8,18 +8,108 @@
 #include "MotorDevice.h"
 
 
-
-
 MotorDevice::MotorDevice(string name):DeviceBase() {
 
-	this->m_name = name;
+	m_name = name;
 	TCommandSchema schema;
 	schema.insert(TCommandSchemaPair("direction","string"));
-	motorOn = new BaseCommand("motorOn",this->m_name,schema);
+	motorOn = new BaseCommand("motorGo",this->m_name,schema);
 	this->addCommand(motorOn);
+	this->OpenGpio();
 }
 
 MotorDevice::~MotorDevice() {
-
+	CloseGpio();
 }
 
+bool MotorDevice::OpenGpio()
+{
+	std::ostringstream directionPath;
+	ofstream directionFile;
+	ofstream exportFile;
+	exportFile.open ("/sys/class/gpio/export");
+
+	if(exportFile.is_open())
+	{
+		exportFile<<MOTOR_L_A_PIN;
+		exportFile<<MOTOR_L_B_PIN;
+		exportFile<<MOTOR_R_A_PIN;
+		exportFile<<MOTOR_R_B_PIN;
+
+		directionPath <<"/sys/class/gpio/"<<MOTOR_L_A_NAME<<"/direction";
+		directionFile.open(directionPath.str());
+		if(!directionFile.is_open())
+			return false;
+		directionFile << "out";
+		directionFile.close();
+
+		directionPath <<"/sys/class/gpio/"<<MOTOR_L_B_NAME<<"/direction";
+		directionFile.open(directionPath.str());
+		if(!directionFile.is_open())
+			return false;
+		directionFile << "out";
+		directionFile.close();
+
+		directionPath <<"/sys/class/gpio/"<<MOTOR_R_A_NAME<<"/direction";
+		directionFile.open(directionPath.str());
+		if(!directionFile.is_open())
+			return false;
+		directionFile << "out";
+		directionFile.close();
+
+		directionPath <<"/sys/class/gpio/"<<MOTOR_R_B_NAME<<"/direction";
+		directionFile.open(directionPath.str());
+		if(!directionFile.is_open())
+			return false;
+		directionFile << "out";
+		directionFile.close();
+
+		return true;
+	}
+
+	exportFile.close();
+
+	return false;
+}
+
+bool MotorDevice::CloseGpio()
+{
+	ofstream unExportFile;
+	unExportFile.open ("/sys/class/gpio/unexport");
+
+	if(unExportFile.is_open())
+	{
+		unExportFile<<MOTOR_L_A_PIN;
+		unExportFile<<MOTOR_L_B_PIN;
+		unExportFile<<MOTOR_R_A_PIN;
+		unExportFile<<MOTOR_R_B_PIN;
+		return true;
+	}
+	return false;
+}
+
+bool MotorDevice::GoOn()
+{
+	ofstream valueFile;
+	std::ostringstream valueFilePath;
+	valueFilePath<<"sys/class/gpio/"<< MOTOR_R_B_NAME<< "/value";
+	valueFile.open(valueFilePath.str());
+	if(valueFile.is_open())
+	{
+		valueFile<<"1";
+
+	}
+	return false;
+}
+bool MotorDevice::GoBack()
+{
+	return false;
+}
+bool MotorDevice::TurnLeft()
+{
+	return false;
+}
+bool MotorDevice::TurnRight()
+{
+	return false;
+}
--- RPINode/src/MotorDevice.h
+++ RPINode/src/MotorDevice.h
@@ -12,12 +12,30 @@
 #include <string>
 #include "BaseCommand.h"
 
+#define MOTOR_L_A_PIN	"53"
+#define MOTOR_L_A_NAME	"pioB21"
+#define MOTOR_L_B_PIN	"55"
+#define MOTOR_L_B_NAME	"pioB23"
+#define MOTOR_R_A_PIN	"59"
+#define MOTOR_R_A_NAME	"pioB27"
+#define MOTOR_R_B_PIN	"47"
+#define MOTOR_R_B_NAME	"pioB15"
+
+
 class MotorDevice: public DeviceBase {
 public:
 	MotorDevice(string name);
 	virtual ~MotorDevice();
 
+	bool GoOn();
+	bool GoBack();
+	bool TurnLeft();
+	bool TurnRight();
+
+
 private:
+	bool OpenGpio();
+	bool CloseGpio();
 	BaseCommand* motorOn;
 };
 
