e7309b324ad57459ed83a8e33ddb2368b27239d4
Cesare Vinciguerra <cesare.vinciguerra@stud.unifi.it>
2016-08-06 17:44:47 +0200

    the rpinode have define for motor pin

--- RPINode/src/RPINode.h
+++ RPINode/src/RPINode.h
@@ -11,6 +11,15 @@
 #include "NodeBase.h"
 #include "MotorDevice.h"
 
+#define MOTOR_L_A_PIN	"53"
+#define MOTOR_L_A_NAME	"pioB21"
+#define MOTOR_L_B_PIN	"55"
+#define MOTOR_L_B_NAME	"pioB23"
+#define MOTOR_R_A_PIN	"59"
+#define MOTOR_R_A_NAME	"pioB27"
+#define MOTOR_R_B_PIN	"47"
+#define MOTOR_R_B_NAME	"pioB15"
+
 class RPINode : public NodeBase {
 public:
 	RPINode();
