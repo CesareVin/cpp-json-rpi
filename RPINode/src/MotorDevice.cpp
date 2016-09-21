2298a7f99ede7f2f97f45a2a12c28ffc381d3974
Cesare Vinciguerra <cesare.vinciguerra@stud.unifi.it>
2016-09-21 23:05:49 +0200

    Motor device is now intened as a full motor driver The driver is able to manage motor direction

--- RPINode/src/MotorDevice.cpp
+++ RPINode/src/MotorDevice.cpp
@@ -11,28 +11,41 @@
 
 MotorDevice::MotorDevice(string name):DeviceBase() {
 
+
 	m_name = name;
-	TCommandSchema schema;
-	schema.insert(TCommandSchemaPair("direction","string"));
+	/*TCommandSchema schema;
+	schema.insert(TCommandSchemaPair("power","string,0->100"));
 	motorOn = new BaseCommand("motorGo",this->m_name,schema);
 	this->addCommand(motorOn);
-	this->OpenGpio();
+	this->OpenGpio();*/
 }
 
-MotorDevice::MotorDevice(string name,string pinA,string nameA,string pinB,string nameB):DeviceBase() {
+MotorDevice::MotorDevice(string name,string LpinA,string LnameA,string LpinB,string LnameB,
+						 string RpinA,string RnameA,string RpinB,string RnameB):DeviceBase() {
 
 	m_name = name;
 	TCommandSchema schema;
-	schema.insert(TCommandSchemaPair("direction","string"));
-	motorOn = new BaseCommand("motorGo",this->m_name,schema);
+	schema.insert(TCommandSchemaPair("power","string,0->100"));
+	motorOn = new BaseCommand("motorOn",this->m_name,schema);
+	motorLeft = new BaseCommand("motorLeft",this->m_name,schema);
+	motorRight = new BaseCommand("motorRight",this->m_name,schema);
+	motorBack = new BaseCommand("motorBack",this->m_name,schema);
 
-	m_motorPinA = pinA;
-	m_motorNameA = nameA;
+	m_motorLPinA = LpinA;
+	m_motorLNameA = LnameA;
+	m_motorLPinB = LpinB;
+	m_motorLNameB = LnameB;
 
-	m_motorPinB = pinB;
-	m_motorNameB = nameB;
+	m_motorRPinA = LpinA;
+	m_motorRNameA = LnameA;
+	m_motorRPinB = LpinB;
+	m_motorRNameB = LnameB;
 
 	this->addCommand(motorOn);
+	this->addCommand(motorLeft);
+	this->addCommand(motorRight);
+	this->addCommand(motorBack);
+
 	this->OpenGpio();
 }
 
@@ -49,17 +62,35 @@ bool MotorDevice::OpenGpio()
 
 	if(exportFile.is_open())
 	{
-		exportFile<<m_motorPinA;
-		exportFile<<m_motorPinB;
-	
-		directionPath <<"/sys/class/gpio/"<<m_motorNameA<<"/direction";
+		//export motor L pins
+		exportFile<<m_motorLPinA;
+		exportFile<<m_motorLPinB;
+		//export motor R pins
+		exportFile<<m_motorRPinA;
+		exportFile<<m_motorRPinB;
+		//setup direction
+		directionPath <<"/sys/class/gpio/"<<m_motorLNameA<<"/direction";
 		directionFile.open(directionPath.str());
 		if(!directionFile.is_open())
 			return false;
 		directionFile << "out";
 		directionFile.close();
 
-		directionPath <<"/sys/class/gpio/"<<m_motorNameB<<"/direction";
+		directionPath <<"/sys/class/gpio/"<<m_motorRNameA<<"/direction";
+		directionFile.open(directionPath.str());
+		if(!directionFile.is_open())
+			return false;
+		directionFile << "out";
+		directionFile.close();
+
+		directionPath <<"/sys/class/gpio/"<<m_motorLNameB<<"/direction";
+		directionFile.open(directionPath.str());
+		if(!directionFile.is_open())
+			return false;
+		directionFile << "out";
+		directionFile.close();
+
+		directionPath <<"/sys/class/gpio/"<<m_motorRNameB<<"/direction";
 		directionFile.open(directionPath.str());
 		if(!directionFile.is_open())
 			return false;
@@ -81,8 +112,10 @@ bool MotorDevice::CloseGpio()
 
 	if(unExportFile.is_open())
 	{
-		unExportFile<<m_motorPinA;
-		unExportFile<<m_motorPinB;
+		unExportFile<<m_motorLPinA;
+		unExportFile<<m_motorLPinB;
+		unExportFile<<m_motorRPinA;
+		unExportFile<<m_motorRPinB;
 		return true;
 	}
 	return false;
@@ -91,13 +124,18 @@ bool MotorDevice::CloseGpio()
 bool MotorDevice::GoOn()
 {
 	ofstream valueFile;
+	ofstream valueFile1;
 	std::ostringstream valueFilePath;
-	valueFilePath<<"sys/class/gpio/"<< m_motorNameA<< "/value";
+	std::ostringstream valueFilePath1;
+	valueFilePath<<"sys/class/gpio/"<< m_motorLNameA<< "/value";
+	valueFilePath1<<"sys/class/gpio/"<< m_motorRNameA<< "/value";
 	valueFile.open(valueFilePath.str());
+	valueFile1.open(valueFilePath1.str());
 	if(valueFile.is_open())
 	{
 		valueFile<<"1";
-
+		valueFile<<"1";
+		return true;
 	}
 	return false;
 }
@@ -113,8 +151,30 @@ bool MotorDevice::TurnRight()
 {
 	return false;
 }
-void MotorDevice::dispatchCommand(BaseCommand& command,Net::Http::ResponseWriter response)
+bool MotorDevice::dispatchCommand(BaseCommand command,Net::Http::ResponseWriter response)
 {
+	if(command == motorOn)
+	{
+		printf("[INFO] motorOn \n");
+		GoOn();
+	}
+	else if(command == motorBack)
+	{
+		printf("[INFO] motorBack \n");
+		GoBack();
+	}
+	else if(command == motorLeft)
+	{
+		printf("[INFO] motorLeft \n");
+		TurnLeft();
+	}
+	else if(command == motorRight)
+	{
+		printf("[INFO] motorLeft \n");
+		TurnRight();
+	}
+	response.send(Net::Http::Code::Ok, "Command Executed");
 
+	return true;
 }
 
